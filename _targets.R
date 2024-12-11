e4f07048f9d42c32d62caf3045d954963287028b
Theresia Maria Giovanna Dronkers <tessadronkers2@gmail.com>
2024-12-11 09:46:15 +0100

    added first target steps: descriptive stats function to targets pipeline

--- _targets.R
+++ _targets.R
@@ -27,7 +27,7 @@ tar_option_set(
   # cluster, select a controller from the {crew.cluster} package.
   # For the cloud, see plugin packages like {crew.aws.batch}.
   # The following example is a controller for Sun Grid Engine (SGE).
-  # 
+  #
   #   controller = crew.cluster::crew_controller_sge(
   #     # Number of workers that the pipeline can scale up to:
   #     workers = 10,
@@ -51,12 +51,21 @@ tar_source()
 # Replace the target list below with your own:
 list(
   tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100))
-    # format = "qs" # Efficient storage for general data objects.
+    name = file,
+    command = "data/lipidomics.csv", # allocates a location
+    format = "file",
   ),
   tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
+    name = lipidomics,
+    command = readr::read_csv(file, show_col_types = FALSE)
+  ),
+  tar_target(
+    name = df_stats_by_metabolite,
+    command = descriptive_stats(lipidomics)
   )
 )
+
+#run it as "targets::tar_make()" - targets will ignore steps it has rerun before
+#visualise using targets::tar_visnetwork()
+#files will be created inside the _targets file under objects
+#run  targets::tar_outdated() to see if anything has been changed
